202d7c9e48d74a156a2b495278541e63c24d0b17
Alistair Hey <alistair@heyal.co.uk>
2022-07-05 12:03:34 +0100

    Add COC Covid Policy and amend schedule
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

---
+++ src/components/CovidPolicy.jsx
@@ -0,0 +1,177 @@
+
+export function CovidPolicy() {
+    return (
+        <div className="relative py-16 bg-white overflow-hidden">
+            <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+                <div className="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
+                    <svg
+                        className="absolute top-12 left-full transform translate-x-32"
+                        width={404}
+                        height={384}
+                        fill="none"
+                        viewBox="0 0 404 384"
+                    >
+                        <defs>
+                            <pattern
+                                id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+                                x={0}
+                                y={0}
+                                width={20}
+                                height={20}
+                                patternUnits="userSpaceOnUse"
+                            >
+                                <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                            </pattern>
+                        </defs>
+                        <rect width={404} height={384} fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+                    </svg>
+                    <svg
+                        className="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
+                        width={404}
+                        height={384}
+                        fill="none"
+                        viewBox="0 0 404 384"
+                    >
+                        <defs>
+                            <pattern
+                                id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                                x={0}
+                                y={0}
+                                width={20}
+                                height={20}
+                                patternUnits="userSpaceOnUse"
+                            >
+                                <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                            </pattern>
+                        </defs>
+                        <rect width={404} height={384} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+                    </svg>
+                    <svg
+                        className="absolute bottom-12 left-full transform translate-x-32"
+                        width={404}
+                        height={384}
+                        fill="none"
+                        viewBox="0 0 404 384"
+                    >
+                        <defs>
+                            <pattern
+                                id="d3eb07ae-5182-43e6-857d-35c643af9034"
+                                x={0}
+                                y={0}
+                                width={20}
+                                height={20}
+                                patternUnits="userSpaceOnUse"
+                            >
+                                <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                            </pattern>
+                        </defs>
+                        <rect width={404} height={384} fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
+                    </svg>
+                </div>
+            </div>
+            <div className="relative px-4 sm:px-6 lg:px-8">
+                <div className="text-lg max-w-prose mx-auto">
+                    <h1>
+            <span className="block text-base text-center text-yellow-600 font-semibold tracking-wide uppercase">
+              Covid Policy
+            </span>
+                        <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              TechMidsConf Covid policy
+            </span>
+                    </h1>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        TechMidsConf is an inclusive event to celebrate and explore the great technical talent across the region, so we don’t want you spending the day anxious about covid, we want you to be having fun!
+                    </p>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        To ensure the event is as safe as possible for all in attendance we are taking the following precautions and ask that you help us by cooperating with them.
+                    </p>
+
+                    <h2>
+                        <span className="mt-2 block text-xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-xl">
+                        On arrival at the event
+            </span>
+                    </h2>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        When you arrive we will do the following:
+                    </p>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        Check your temperature
+                    </p>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        We will do this by using a “thermometer gun,” a device that is equipped with an infrared sensor that can quickly measure surface temperature without making any contact with a person&rsquo;s skin.
+                    </p>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        Ask you to provide proof of at least three Covid vaccinations
+                    </p>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        You can do this via the paper card you carry in your wallet or by showing us your digital “NHS Covid Pass”.
+                        To get the NHS Covid Pass you’ll need to register on the NHS App and download it to your phone ahead of time. To register, the process takes around 5-10 mins but it can take up to 24 hours for the NHS to process so please do this ahead of time. You can then save the pass into your digital wallet for easy access.
+                    </p>
+                    <li className="mt-8 text-xl text-gray-500 leading-8">International attendees should provide an equivalent certification from their country.</li>
+                    <li className="mt-8 text-xl text-gray-500 leading-8">If you have medical reasons why you have not been vaccinated please let the volunteers know. We understand there will need to be exemptions for some cases.</li>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        We ask you to take a lateral flow test prior to attending the event
+                    </p>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        While we are not requiring proof of negative lateral flow at the event we kindly ask that you take a test before attending the
+                        event both days for everyone&rsquo;s safety.
+                    </p>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        Lateral flow tests can be ordered online from most pharmacies.
+                    </p>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        Ask you to choose a wrist band.
+                    </p>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        The wrist band will indicate if you are socially distancing or not.
+                    </p>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        Red band: Please keep your distance.
+                    </p>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+
+                        Green band:  I&rsquo;m not socially distancing.
+                    </p>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        We thank you in advance for being respectful about peoples choices around their social distancing requirements.
+                    </p>
+                    <h2>
+                        <span className="mt-2 block text-xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-xl">
+                        Throughout the conference
+            </span>
+                    </h2>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        It is not a socially distanced conference so there is no requirement for you to keep your distance from your friends, but please be respectful of others who may want to keep their distance.
+
+                        Face mask wearing will be optional, depending on your comfort levels. We strongly encourage mask wearing during the event.
+                    </p>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        We will provide hand sanitiser around the venue for use throughout the day
+                    </p>
+
+                    <h2>
+                        <span className="mt-2 block text-xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-xl">
+                        Thank you for your patience
+            </span>
+                    </h2>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        These additional checks will mean it takes a little longer to check you in than we would usually like, but hope you understand we are taking these precautions so that everyone feels safe and relaxed and can enjoy the event.
+                        These additional checks will mean it takes a little longer to check you in than we would usually like, but hope you understand we are taking these precautions so that everyone feels safe and relaxed and can enjoy the event.
+                    </p>
+                    <h2>
+                        <span className="mt-2 block text-xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-xl">
+                        Changes
+            </span>
+                    </h2>
+                    <h2>
+                    </h2>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        These additional checks will mean it takes a little longer to check you in than we would usually like, but hope you understand we are taking these precautions so that everyone feels safe and relaxed and can enjoy the event.
+                        We may need to amend these policies due to local or national guidance or legislation. We will be in touch prior to the event
+                        to confirm the COVID requirements.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
