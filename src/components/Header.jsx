eae16da11b4b57f08999e806c8af1a7651f79d92
Luke Browne <luke.browne@wealthwizards.com>
2022-07-14 22:02:25 +0100

    fix: made date bar work on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,14 +12,13 @@ export function Header() {
           </a>
         </div>
         <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-yellow-600/10 py-4 font-mono text-sm text-yellow-600 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
-          <div className="mx-auto flex items-center space-x-4 px-4">
+          <div className="mx-auto flex flex-col items-center space-x-4 px-4 md:flex-row">
             <p>
-              <time dateTime="2022-04-04">14th </time>
-              <time dateTime="2022-04-06">October, 2022</time>
+              <time dateTime="2022-10-14">14th October 2022</time>
             </p>
             <svg
               aria-hidden="true"
-              className="h-1.5 w-1.5 overflow-visible fill-current stroke-current"
+              className="invisible h-1.5 w-1.5 overflow-visible fill-current stroke-current md:visible"
             >
               <path
                 d="M3 0L6 3L3 6L0 3Z"
