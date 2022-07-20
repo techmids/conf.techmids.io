c6ff98f3da0a5184aa8a061e8eea2550e60eb7df
Luke Browne <luke.browne@wealthwizards.com>
2022-07-20 14:29:15 +0100

    chore: fix Kunal's photo

--- speakers.js
+++ speakers.js
@@ -1,17 +1,17 @@
-import kunalImg from "@/images/avatars/kunal.png";
+import KunalImage from '@/images/avatars/kunal.png'
 
 const speakers = [
-    {
-        name: "Kunal Kushwaha",
-        jobTitle: "Developer Advocate at Civo",
-        bio: "",
-        talkTitle: "Check back later to see",
-        description: "This talk is not yet ready to be published",
-        image: kunalImg,
-        twitter: "kunalstwt",
-        linkedin: "",
-        website: "https://www.kunalkushwaha.com/"
-    },
+  {
+    name: 'Kunal Kushwaha',
+    jobTitle: 'Developer Advocate at Civo',
+    bio: '',
+    talkTitle: 'Coming soon...',
+    description: 'This talk will be announced soon',
+    image: KunalImage,
+    twitter: 'kunalstwt',
+    linkedin: '',
+    website: 'https://www.kunalkushwaha.com/',
+  },
 ]
 
 export default speakers
