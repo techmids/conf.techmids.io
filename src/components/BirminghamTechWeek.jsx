49fc86ac438a10323327149927c164ceefd9fdc1
Tom Withers <tomtucka@gmail.com>
2022-07-20 12:46:04 +0100

    feat: remove btw blue background (#6)
    * remove btw blue background
    
    * add black btw logo
    
    * wrap content in div
    
    * fix: revert back to no wrapping div
    
    * fix: remove all classes from image accept mx-auto

--- src/components/BirminghamTechWeek.jsx
+++ src/components/BirminghamTechWeek.jsx
@@ -1,13 +1,13 @@
 export function BirminghamTechWeek() {
   return (
-    <section className="mt-20 bg-btw-blue py-10 lg:px-12">
-      <h3 className="mb-10 text-center font-display text-4xl font-medium tracking-tighter text-white">
+    <section className="mt-20 bg-white py-10 lg:px-12">
+      <h3 className="mb-16 text-center font-display text-4xl font-medium tracking-tighter text-yellow-900 sm:text-5xl">
         TechMids Conf is proudly part of
       </h3>
       <img
-        src="/birmingham-tech-week.png"
+        src="/birmingham-tech-week-black.png"
         alt="Birmingham Tech Week"
-        className="mx-auto w-64 sm:max-h-24 sm:w-fit"
+        className="mx-auto"
       />
     </section>
   )
