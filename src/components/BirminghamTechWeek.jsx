7f83e2bab215c213ab2d8f5a0c16c92ec286ea68
Tom Withers <tomtucka@gmail.com>
2022-07-20 11:37:00 +0000

    wrap content in div

--- src/components/BirminghamTechWeek.jsx
+++ src/components/BirminghamTechWeek.jsx
@@ -1,14 +1,16 @@
 export function BirminghamTechWeek() {
   return (
     <section className="mt-20 bg-white py-10 lg:px-12">
-      <h3 className="mb-16 text-center font-display text-4xl font-medium tracking-tighter text-yellow-900 sm:text-5xl">
-        TechMids Conf is proudly part of
-      </h3>
-      <img
-        src="/birmingham-tech-week-black.png"
-        alt="Birmingham Tech Week"
-        className="mx-auto w-64 sm:max-h-24 sm:w-fit grayscale"
-      />
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative">
+        <h3 className="mb-16 text-center font-display text-4xl font-medium tracking-tighter text-yellow-900 sm:text-5xl">
+          TechMids Conf is proudly part of
+        </h3>
+        <img
+          src="/birmingham-tech-week-black.png"
+          alt="Birmingham Tech Week"
+          className="mx-auto w-64 sm:max-h-24 sm:w-fit grayscale"
+        />
+      </div>
     </section>
   )
 }
