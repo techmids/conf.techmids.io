528dd291580b9cbba7ab936d4852f27373c88163
Luke Browne <luke.browne@wealthwizards.com>
2022-07-25 09:13:13 +0100

    feat: add Kunal talk info

--- speakers.js
+++ speakers.js
@@ -2,14 +2,14 @@ import KunalImage from '@/images/avatars/kunal.png'
 
 const speakers = [
   {
-    name: 'Kunal Kushwaha',
-    jobTitle: 'Developer Advocate at Civo',
-    bio: '',
-    talkTitle: 'Coming soon...',
-    description: 'This talk will be announced soon',
-    image: KunalImage,
-    twitter: 'kunalstwt',
-    linkedin: '',
+    name: "Kunal Kushwaha",
+    jobTitle: "Developer Advocate",
+    bio: "Kunal is working towards empowering communities via open-source and education. He finds passion in teaching and has taught thousands of folks online and in person. He is currently a Developer Advocate at Civo, CNCF Ambassador, track chair of the KubeCon + CloudNativeCon student track, Major League Hacking Coach. He is the founder of Kubeworld, Community Classroom, and also started the official Cloud Native Student Community group joined by thousands of students, focussed on getting more young people involved in the ecosystem.",
+    talkTitle: "Scaling Communities to be more Inclusive",
+    description: "Being an open-source enthusiast, Kunal believes that diversity in the workplace and participation from people hailing from different cultures is necessary as well as instrumental for the growth of the IT sector. It exposes one to the multitude of values and principles that people from varying ethnicities hold. Meeting people from around the world teaches people to respect opposing perspectives and opinions, and ingrains in them respect for their peers. The talk is going to be focussed around what defines a community, and figuring out what are the community's shared struggles. It’s also important to know what is the mission of your community and what members look to get out of it. Communication is key and we’ll also talk about how to future proof your community. Regarding diversity and inclusion, it’s important to know who might be excluded from accessing your community activities in their current form. We’ll also discuss about what are some of the negative scenarios which might happen while running activities for your community which will make them less inclusive to marginalised groups. Following up with designing for your community's needs, and last but not least, having a Code of Conduct. Attendees will learn about: How to start a new community and make it inclusive from the beginning. How to scale communities to under-representative groups How to deal with conflicts and take care of your audience’s needs.",
+    speakerImg: KunalImage,
+    twitter: "https://twitter.com/kunalstwt",
+    linkedin: "https://linkedin.com/in/kunal-kushwaha/",
     website: 'https://www.kunalkushwaha.com/',
   },
 ]
