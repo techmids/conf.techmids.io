108ab79536bfff6fa0d4b5dad4054ebf9ee14f7b
Luke Browne <luke.browne@wealthwizards.com>
2022-07-25 09:26:07 +0100

    fix: links

--- speakers.js
+++ speakers.js
@@ -8,8 +8,8 @@ const speakers = [
     talkTitle: "Scaling Communities to be more Inclusive",
     description: "Being an open-source enthusiast, Kunal believes that diversity in the workplace and participation from people hailing from different cultures is necessary as well as instrumental for the growth of the IT sector. It exposes one to the multitude of values and principles that people from varying ethnicities hold. Meeting people from around the world teaches people to respect opposing perspectives and opinions, and ingrains in them respect for their peers.  \n\nThe talk is going to be focussed around what defines a community, and figuring out what are the community's shared struggles. It’s also important to know what is the mission of your community and what members look to get out of it. Communication is key and we’ll also talk about how to future proof your community.  \n\nRegarding diversity and inclusion, it’s important to know who might be excluded from accessing your community activities in their current form. We’ll also discuss about what are some of the negative scenarios which might happen while running activities for your community which will make them less inclusive to marginalised groups.  \n\nFollowing up with designing for your community's needs, and last but not least, having a Code of Conduct.  \n\nAttendees will learn about: How to start a new community and make it inclusive from the beginning. How to scale communities to under-representative groups How to deal with conflicts and take care of your audience’s needs.",
     image: KunalImage,
-    twitter: "https://twitter.com/kunalstwt",
-    linkedin: "https://linkedin.com/in/kunal-kushwaha/",
+    twitter: "kunalstwt",
+    linkedin: "kunal-kushwaha",
     website: 'https://www.kunalkushwaha.com/',
   },
 ]
