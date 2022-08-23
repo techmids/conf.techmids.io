8a1a9aeeae7508f78ed01c3aa7d50b249316d992
Luke Browne <luke.browne@wealthwizards.com>
2022-08-23 16:40:35 +0100

    feat: bump Jump24 to Gold sponsorship

--- src/components/SponsorTiers.jsx
+++ src/components/SponsorTiers.jsx
@@ -26,7 +26,7 @@ export function SponsorTiers() {
       </div>
       <div className="mx-auto max-w-6xl py-8 px-4 sm:px-4 lg:py-12 lg:px-4">
         <p className="text-center text-base font-semibold uppercase tracking-wider text-gray-600">
-          Silver Sponsors
+          Gold Sponsors
         </p>
         <div className="mt-6 grid grid-cols-1 gap-0.5 lg:mt-8">
           <div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
