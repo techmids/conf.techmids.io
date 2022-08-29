6e5502b5d8b7f2e4a22320c1b3525f1b74643a88
Luke Browne <luke.browne@wealthwizards.com>
2022-08-29 13:26:58 +0100

    fix: container on mobile

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,5 +1,10 @@
 import clsx from 'clsx'
 
 export function Container({ className, ...props }) {
-  return <div className={clsx('mx-auto max-w-6xl', className)} {...props} />
+  return (
+    <div
+      className={clsx('mx-auto max-w-6xl px-4 sm:px-6 lg:px-8', className)}
+      {...props}
+    />
+  )
 }
