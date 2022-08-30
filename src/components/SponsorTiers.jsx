864d2f293e056955269e64fd2684b3e86c704210
Luke Browne <luke.browne@wealthwizards.com>
2022-08-30 15:21:10 +0100

    feat: add Infinity Works as silver sponsor

--- src/components/SponsorTiers.jsx
+++ src/components/SponsorTiers.jsx
@@ -1,7 +1,7 @@
 export function SponsorTiers() {
   return (
-    <div className="mt-16 mb-8 bg-white lg:mt-0">
-      <div className="py-8 lg:py-12">
+    <div className="mt-12 mb-8 bg-white">
+      <div className="py-8">
         <p className="text-center text-base font-semibold uppercase tracking-wider text-gray-600">
           Gold Sponsors
         </p>
@@ -9,6 +9,26 @@ export function SponsorTiers() {
           <div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
             <a href="https://jump24.co.uk/" target="_blank" rel="noopener">
               <img className="max-h-12" src="/jump24.png" alt="Jump24" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="py-8">
+        <p className="text-center text-base font-semibold uppercase tracking-wider text-gray-600">
+          Silver Sponsors
+        </p>
+        <div className="mt-6 grid grid-cols-1 gap-0.5 lg:mt-8">
+          <div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+            <a
+              href="https://www.infinityworks.com/"
+              target="_blank"
+              rel="noopener"
+            >
+              <img
+                className="max-h-12"
+                src="/infinityworks.png"
+                alt="Infinity Works"
+              />
             </a>
           </div>
         </div>
