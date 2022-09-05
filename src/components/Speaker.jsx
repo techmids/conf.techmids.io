78ae95a034412e882804afb01828d43097b052b1
Jack Browne <jackbrowne00@gmail.com>
2022-09-05 19:30:11 +0100

    feat: nic and ben pages added

--- src/components/Speaker.jsx
+++ src/components/Speaker.jsx
@@ -14,11 +14,13 @@ export function Speaker(props) {
       twitter,
       linkedin,
       website,
+      youtube,
       fullSession,
     },
   } = props
   const twitterURL = `https://twitter.com/${twitter}`
   const linkedinURL = `https://www.linkedin.com/in/${linkedin}`
+  const youtubeURL = `https://www.youtube.com/c/${youtube}`
 
   return (
     <div className="bg-white pt-4 lg:py-12">
@@ -126,6 +128,26 @@ export function Speaker(props) {
                               d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
                               clipRule="evenodd"
                             />
+                          </svg>
+                        </a>
+                      </li>
+                    ) : null}
+                    {youtube && youtube !== '' ? (
+                      <li>
+                        <a
+                          href={youtubeURL}
+                          className="text-yellow-800"
+                          rel="noreferrer noopener"
+                          target="_blank"
+                        >
+                          <span className="sr-only">YouTube</span>
+                          <svg
+                            className="h-5 w-5"
+                            aria-hidden="true"
+                            fill="currentColor"
+                            viewBox="0 0 576 512"
+                          >
+                            <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" />
                           </svg>
                         </a>
                       </li>
