015b05f41b682d6f2a674b869b283f7170c16266
Luke Browne <luke.browne@wealthwizards.com>
2022-10-14 08:40:12 +0100

    feat: add live stream

--- src/components/ButtonCTA.jsx
+++ src/components/ButtonCTA.jsx
@@ -5,12 +5,12 @@ export function ButtonCTA() {
     <>
       <div className="my-10 grid grid-cols-1 gap-10">
         <ButtonLink
-          href="https://ti.to/tech-events-birmingham/tech-mids-conference-2022"
+          href="https://www.youtube.com/watch?v=LUKr9GQphOY"
           rel="noopener"
           target="_blank"
           className="w-full"
         >
-          Get your free tickets
+          Join the stream from 9am
         </ButtonLink>
       </div>
       <dl className="mt-10 grid grid-cols-2 gap-y-6 gap-x-10 sm:mt-16 sm:gap-y-10 sm:gap-x-16 sm:text-center lg:auto-cols-auto lg:grid-flow-col lg:grid-cols-none lg:justify-start lg:text-left">
