811590e7dcf4b4aa753b65a0a831cba6c58815e9
Alistair Hey <alistair@heyal.co.uk>
2023-05-09 16:12:55 +0100

    Add sponsorship section
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- src/pages/sponsorship.jsx
+++ src/pages/sponsorship.jsx
@@ -41,7 +41,6 @@ const tiers = [
         features: [
             'Ability to leave your stickers/swag at our swag drop table',
             'Your logo on our Conference Badges',
-            'Two free tickets',
         ],
         featured: false,
     },
@@ -134,6 +133,51 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
+const CredentialsSection = () => {
+    const stats = [
+        { id: 1, name: 'Combined (all years) Event Attendees', value: '1,000+' },
+        { id: 2, name: 'Previous Sponsors', value: '35+' },
+        { id: 3, name: 'Total Speakers', value: '60+' },
+        { id: 4, name: 'Combined Days', value: '5' },
+    ]
+
+return (
+    <div className="relative bg-white pt-6">
+        <img
+            className="rounded-3xl h-56 w-full bg-gray-50 object-cover lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-1/2"
+            src="/mp-leon-adato.jpg"
+            alt=""
+        />
+        <div className="mx-auto grid max-w-7xl lg:grid-cols-2">
+            <div className="px-6 pb-24 pt-16 sm:pb-32 sm:pt-20 lg:col-start-2 lg:px-8 lg:pt-32">
+                <div className="mx-auto max-w-2xl lg:mr-0 lg:max-w-lg">
+                    <h2 className="text-base font-semibold leading-8 text-yellow-600">Our track record</h2>
+                    <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                        4 Amazing events over 3 years
+                    </p>
+                    <p className="mt-6 text-lg leading-8 text-gray-600">
+                        The team behind TechMids have run 4 non-profit conferences over the last 3 years. We have a great reputation
+                        in the community and are the 'Must Attend' event of the year. (A
+                    </p>
+                        <p className="mt-6 text-lg leading-8 text-gray-600">
+                        Each year we sell out, filling the 350+ seater venue.
+                    </p>
+                    <dl className="mt-16 grid max-w-xl grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 xl:mt-16">
+                        {stats.map((stat) => (
+                            <div key={stat.id} className="flex flex-col gap-y-3 border-l border-gray-900/10 pl-6">
+                                <dt className="text-sm leading-6 text-gray-600">{stat.name}</dt>
+                                <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900">{stat.value}</dd>
+                            </div>
+                        ))}
+                    </dl>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+}
+
+
 export default function Sponsorship() {
     return (
         <>
@@ -161,6 +205,8 @@ export default function Sponsorship() {
                         {reasons.map((reason) => (
                             <li className="mx-auto max-w-4xl text-left text-lg leading-8 text-gray-600">{reason}</li>
                         ))}
+
+                        <CredentialsSection/>
 
                         <div className="mx-auto max-w-4xl pt-6 text-center">
                             <p className="mt-2 text-xl font-bold tracking-tight text-gray-900 sm:text-3xl">
