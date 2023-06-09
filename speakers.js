94623030319e0325c2612f807dfc2688c89b3503
Alistair Hey <alistair@heyal.co.uk>
2023-06-09 16:36:52 +0100

    Add amanda's talk
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- speakers.js
+++ speakers.js
@@ -1170,10 +1170,15 @@ export const Sessions= {
             </>
         ),
         talkTitle:
-            "",
+            "Will Open Source Fail?",
         description: (
             <>
                 <p>
+                    Will Open Source Fail?
+                    In this talk Amanda will look at the position of open source software in the global marketplace and the UK today, the challenges it faces and and what the future of open source software is.
+                </p>
+                <p>
+                    The audience will take away a better understanding of what open source software is, how it’s utilisation has grown to be the majority of software today, specific challenges it faces today and where it’s future could lie
                 </p>
             </>
         ),
