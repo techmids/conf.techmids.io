c8583b575bed2649797306aa85e1ea5f40715e84
Alistair Hey <alistair@heyal.co.uk>
2023-07-27 15:47:57 +0100

    Add domain validation header
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,24 +1,26 @@
 import { Head, Html, Main, NextScript } from 'next/document'
+import {Meta} from "next/dist/lib/metadata/generate/meta";
 
 export default function Document() {
   return (
-    <Html className="bg-white antialiased" lang="en">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=DM+Sans:wght@400;500;700&display=swap"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+      <Html className="bg-white antialiased" lang="en">
+          <Head>
+              <Meta name="google-site-verification" content="GdmzzVKmmOb3Y48Uze04WSJZStzlLo28q2RRd_lyZvI" />
+              <link rel="preconnect" href="https://fonts.googleapis.com" />
+              <link
+                  rel="preconnect"
+                  href="https://fonts.gstatic.com"
+                  crossOrigin="anonymous"
+              />
+              <link
+                  rel="stylesheet"
+                  href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=DM+Sans:wght@400;500;700&display=swap"
+              />
+          </Head>
+          <body>
+          <Main />
+          <NextScript />
+          </body>
+      </Html>
   )
 }
