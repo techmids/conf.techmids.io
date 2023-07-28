b904a1901e5dfb5bf8b92ca523a95de3dc5ce406
Alistair Hey <alistair@heyal.co.uk>
2023-07-28 14:04:51 +0100

    change sponsors page
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- src/pages/sponsorship.jsx
+++ src/pages/sponsorship.jsx
@@ -145,11 +145,6 @@ const reasons = [
     { description: "Run by a team of volunteers, we are a not-for-profit event", icon: GlobeIcon},
     { description: "Associate yourself with a great event and a great community", icon: CheckIcon},
 
-    // "We are the best way to get in front of the West Midlands developer community",
-    // "We sell out of tickets every year",
-    // "We have a great reputation in the community",
-    // "Run by a team of volunteers, we are a not-for-profit event",
-    // "Associate yourself with a great event and a great community",
 ]
 
 function classNames(...classes) {
@@ -158,42 +153,49 @@ function classNames(...classes) {
 
 const CredentialsSection = () => {
     const stats = [
-        { id: 1, name: 'Combined (all years) Event Attendees', value: '1,000+' },
+        { id: 1, name: 'Combined Event Attendees', value: '1,000+' },
         { id: 2, name: 'Previous Sponsors', value: '35+' },
         { id: 3, name: 'Total Speakers', value: '60+' },
     ]
 
 return (
-    <div className="mt-5 relative bg-white pt-6">
+    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
         <img
-            className="rounded-3xl h-56 w-full bg-gray-50 object-cover lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-1/2"
-            src="/gallery/mp-leon-adato.jpg"
+            src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2850&q=80&blend=111827&blend-mode=multiply&sat=-100&exp=15"
             alt=""
+            className="absolute inset-0 -z-10 h-full w-full object-cover"
         />
-        <div className="mx-auto grid max-w-7xl lg:grid-cols-2">
-            <div className="px-6 pb-24 pt-16 sm:pb-32 sm:pt-20 lg:col-start-2 lg:px-8 lg:pt-32">
-                <div className="mx-auto max-w-2xl lg:mr-0 lg:max-w-lg">
-                    <h2 className="text-base font-semibold leading-8 text-brand-900">Our track record</h2>
-                    <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                        4 Amazing events over 3 years
-                    </p>
-                    <p className="mt-6 text-lg leading-8 text-gray-600">
-                        The team behind TechMids have run 4 non-profit conferences over the last 3 years. We have a great reputation
-                        in the community and are the 'Must Attend' event of the year.
-                    </p>
-                        <p className="mt-6 text-lg leading-8 text-gray-600">
-                        Each year we sell out
-                    </p>
-                    <dl className="mt-16 grid max-w-xl grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 xl:mt-16">
-                        {stats.map((stat) => (
-                            <div key={stat.id} className="flex flex-col gap-y-3 border-l border-gray-900/10 pl-6">
-                                <dt className="text-sm leading-6 text-gray-600">{stat.name}</dt>
-                                <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900">{stat.value}</dd>
-                            </div>
-                        ))}
-                    </dl>
-                </div>
+        <div className="relative mx-auto max-w-7xl px-6 lg:px-8">
+            <div
+                className="absolute -bottom-8 -left-96 -z-10 transform-gpu blur-3xl sm:-bottom-64 sm:-left-40 lg:-bottom-32 lg:left-8 xl:-left-10"
+                aria-hidden="true"
+            >
+                <div
+                    className="aspect-[1266/975] w-[79.125rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+                    style={{
+                        clipPath:
+                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                    }}
+                />
             </div>
+            <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
+                <h2 className="text-base font-semibold leading-8 text-brand-500">Our track record</h2>
+                <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                    4 Amazing conferences over 3 years
+                </p>
+                <p className="mt-6 text-lg leading-8 text-gray-300">
+                    The team behind TechMids have run 4 non-profit conferences over the last 3 years. We have a great reputation
+                    in the community and are the 'Must Attend' event of the year.
+                </p>
+            </div>
+            <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 text-white sm:mt-20 sm:grid-cols-2 sm:gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+                {stats.map((stat) => (
+                    <div key={stat.id} className="flex flex-col gap-y-3 border-l border-white/10 pl-6">
+                        <dt className="text-sm leading-6">{stat.name}</dt>
+                        <dd className="order-first text-3xl font-semibold tracking-tight">{stat.value}</dd>
+                    </div>
+                ))}
+            </dl>
         </div>
     </div>
 )
@@ -240,6 +242,7 @@ export default function Sponsorship() {
             <Header />
             <main>
                 {Reasons()}
+                <CredentialsSection/>
                 <div className="bg-white py-12 sm:py-24">
                     <div className="mx-auto px-6 lg:px-8 lg:max-w-[75%]">
                         {/*<div className="mx-auto max-w-4xl text-center">*/}
@@ -255,7 +258,6 @@ export default function Sponsorship() {
                         {/*    <li className="mx-auto max-w-4xl text-left text-lg leading-8 text-gray-600">{reason}</li>*/}
                         {/*))}*/}
 
-                        <CredentialsSection/>
 
                         <div className="mx-auto max-w-4xl pt-6 text-center">
                             <p className="mt-2 text-xl font-bold tracking-tight text-gray-900 sm:text-3xl">
@@ -353,7 +355,6 @@ export default function Sponsorship() {
                         </p>
                     </div>
                     <Images/>
-                    <SponsorUsCTA/>
                     <Sponsors/>
                 </div>
             </main>
