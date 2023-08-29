a15257c3328286174adb1640d02da1e2e659edc8
Alistair Hey <alistair@heyal.co.uk>
2023-08-29 14:27:20 +0100

    Add Talk descriptions
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- speakers.js
+++ speakers.js
@@ -18,6 +18,20 @@ export const Sessions= {
         description: (
             <>
                 <p>
+                    I've always been interested in the composition of Open Source and internal dependencies that my projects use, and after years of wanting some way to query the data, I went ahead and built it!
+                </p>
+                <p>
+
+                    Having access to this data has allowed me and my team to get a view of what our most popular languages or frameworks are, which packages we didn't realise we so heavily rely on, determine how many dependencies are using end-of-life software, or just understand the way that internally-built libraries are used across the organisation.
+                </p>
+                <p>
+                    This data has given my company's developers, leadership and the security team a better view of the landscape of our ever growing dependencies on Open Source, so we can appropriately upgrade, migrate, and support projects.
+                </p>
+                <p>
+                    In this talk, you'll learn that it's straightforward to do this yourself with Free and Open Source Software, as well as looking at some examples of the data that you can get out of this tooling for your own purposes.
+                </p>
+                <p>
+                    https://talks.jvt.me/dmd/
                 </p>
             </>
         ),
@@ -41,6 +55,10 @@ export const Sessions= {
         description: (
             <>
                 <p>
+                    Effective content modeling is essential for creating a flawless user experience that drives product success. However, as content complexity and volume increase in a company, managing it all can seem overwhelming.
+                </p>
+                <p>
+                    In this talk, developers will walk away with practical strategies for rethinking their content model to improve maintainability, developer experience and scalability - ultimately reducing technical debt and delivering a seamless user experience.
                 </p>
             </>
         ),
@@ -63,6 +81,10 @@ export const Sessions= {
         description: (
             <>
                 <p>
+                    In today's era of digital ecosystems, the role of SDKs has become pivotal in ensuring that APIs are accessible, usable, and provide value to their intended audience: developers. While auto-generation of SDKs may seem a time-efficient approach, it often falls short in terms of usability and developer experience. In this talk, we delve deep into the craft of building SDKs that are not just functional, but truly fantastic.
+                </p>
+                <p>
+                    We'll commence by dissecting the limitations of auto-generated SDKs, underscoring how they might inadvertently hamper developer adoption and experience. By examining real-world case studies, attendees will gain insights into the successes and pitfalls experienced by others in the SDK development realm.
                 </p>
             </>
         ),
